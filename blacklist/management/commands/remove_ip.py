c3d3e5ffc7d04576a8e2a20aef4f8c6fec2580a0
Michael Leer-Hipple <git@trozzy.net>
2021-01-02 03:02:25 +0000

    add command to remove IP

---
+++ blacklist/management/commands/remove_ip.py
@@ -0,0 +1,27 @@
+from datetime import timedelta
+
+from django.core.management.base import BaseCommand
+from django.db.models import F
+from django.utils.timezone import now
+
+from ...models import Rule
+
+
+class Command(BaseCommand):
+    help = 'Trims the blacklist'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--ip', type=string,
+            help='created days ago; default: 0')
+
+    def handle(self, *args, **options):
+        self.stdout.write('Deleting expired rules')
+
+        ip = timedelta(days=options['ip'])
+
+        rules = Rule.objects.filter(address=(ip))
+
+        deleted = rules.delete()
+        num_deleted = deleted[1].get('%s.%s' % (Rule._meta.app_label, Rule._meta.object_name), 0)
+
+        self.stdout.write(self.style.SUCCESS('Deleted %d rule(s).' % num_deleted))
