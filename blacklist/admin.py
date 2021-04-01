b6af13609fd143e6421445b9fd5dde60c9c6b586
itmagistr <itmagistr@gmail.com>
2021-04-02 02:14:41 +0300

    added second migration

--- blacklist/admin.py
+++ blacklist/admin.py
@@ -4,8 +4,8 @@ from .models import Rule
 
 
 class RuleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'address', 'prefixlen', 'created', 'duration', 'is_active')
-    list_filter = ('created', 'duration')
+    list_display = ('id', 'user', 'address', 'prefixlen', 'created', 'expire', 'duration', 'is_active')
+    list_filter = ('created', 'expire', 'duration')
     search_fields = ('user__username', 'address', 'comments')
 
 
